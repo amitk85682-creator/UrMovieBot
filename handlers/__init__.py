724e092e72401846d3372158fedb2fc5df8b400f
amitk85682-creator <amitk85682@gmail.com>
2026-08-18 20:29:51 +0530

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .start import start
-from .search import search
-from .buttons import buttons
-from .group import group_listener
-from .errors import err
--- handlers/errors.py
+++
@@ -1,4 +0,0 @@
-import logging
-log=logging.getLogger(__name__)
-async def err(update,context):
-    log.error("Update caused error", exc_info=context.error)
